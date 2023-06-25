26e084ac8a74768c33f4cabbc3ad7cfef5af855e
Guido Benetti <guidox.alucard@gmail.com>
2023-06-25 03:44:15 -0300

    comit de base de datos

--- TIF_GRUPO_01_DB_AV20/INSERTS_DEVFLIX.sql
+++ TIF_GRUPO_01_DB_AV20/INSERTS_DEVFLIX.sql
@@ -1,6 +1,7 @@
 USE DevFlixDB
 go
-INSERT INTO Paises (IDPais_Pa,Nombre_PA)
+
+INSERT INTO Paises (IDPais_Pa,Nombre_PA)
 select 'ARG','Argentina' union
 select 'BRA','Brasil' union
 select 'URU','Uruguay'
@@ -129,3 +130,6 @@ SELECT 'P018', 'G003', 'PUNIVERSAL','Vinland Saga es una serie anime que gira en
 3, 'Recursos/Imagenes/Portadas/Serie2Accion_VINLANDSAGA.JPG', 
 'Vinland Saga', 0, 'https://www.youtube.com/watch?v=n2VuKI0yT8w', 16, 1 
 GO
+
+select * from Catalogos
+go
