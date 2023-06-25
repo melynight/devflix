c7557b9e27a67a9f0e8b9935523cb2ecf4d0a040
LAPTOP-5OMBHCP7\zoero <zoerouco@gmail.com>
2023-06-25 20:07:51 -0300

    ingreso arreglado

--- Vistas/Ingreso.aspx.cs
+++ Vistas/Ingreso.aspx.cs
@@ -44,22 +44,12 @@ namespace Vistas
                 cuenta.SetCodSus_Cu(sus);
                 cuenta.Set_Pais_Cu(pais);
 
-<<<<<<< HEAD
                 Session["Cuenta"] = cuenta;
                 Application["tipoSuscripcion"] = tip;
                 Session["NombreSus"] = tip.Nombre_Ts1;
 
                 Session["EdadUsuario"] = cuenta.GetEdad_Cu();
                 Session["IDAdmin"] = cuenta.GetIDCuenta();
-                Session["EdadUsuario"] = cuenta.GetEdad_Cu();
-=======
-
-                Session["Cuenta"] = cuenta;
-                Application["tipoSuscripcion"] = tip;
-                Session["NombreSus"] = tip.Nombre_Ts1;
-                Session["EdadUsuario"] = cuenta.GetEdad_Cu();
-                Session["IDAdmin"] = cuenta.GetIDCuenta();
->>>>>>> origin/main
 
                 lblError.Visible = false;
                 txtClave.Text = "";
